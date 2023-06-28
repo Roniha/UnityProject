04d176241f8c4950ac4efa3cb45bff08f4d38f8d
dayzrs <matsuyamadayz@gmail.com>
2023-06-28 11:50:29 +0900

    アニメーションテスト

---
+++ Assets/Scripts/animetion.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    private Animator anim;// Animatorを使うための変数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();// animにAnimatorの値を取得して代入
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))// もしWSADキーのいずれかが押されたら、
+
+        {
+            anim.SetBool("isRunning", true);// AnimatorのisRunningをtrueにする
+        }
+
+    }
+}
