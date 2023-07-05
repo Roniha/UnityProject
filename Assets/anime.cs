ea2ae8c2b232bb17804274cd90ce690f096699f6
dayzrs <matsuyamadayz@gmail.com>
2023-07-05 11:35:44 +0900

    左右に歩くアニメーション

--- Assets/anime.cs
+++ Assets/anime.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class anime : MonoBehaviour
 {
-    private Animator anim;// Animatorを使うための変数
+    private Animator anim; // Animatorを使うための変数
+    private float originalX; // 初期のx座標を保存するための変数
+    private float distance = 5.0f; // 左右する距離
+    private float speed = 2.0f; // 左右するスピード
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();// animにAnimatorの値を取得して代入
+        anim = GetComponent<Animator>(); // animにAnimatorの値を取得して代入
+        anim.SetBool("isWalking", true); // AnimatorのisWalkingをtrueにする
+        originalX = transform.position.x; // originalXに現在のx座標を保存
+        transform.Rotate(0, 90, 0);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))// もしWSADキーのいずれかが押されたら、
+    {   
+        transform.position += transform.forward * 0.04f; // 前方に進む
 
+        // 現在のx座標がoriginalXからdistance分以上離れた場合、または0以下の場合に90度回転
+        if (transform.position.x <= 0 || transform.position.x >= originalX + distance)
         {
-            anim.SetBool("isRunning", true);// AnimatorのisRunningをtrueにする
-        }
+            // 90度回転させる
+            transform.Rotate(0, 180, 0);
 
+            // 新しいx座標をoriginalXとして保存
+            originalX = transform.position.x;
+        }
     }
 }
